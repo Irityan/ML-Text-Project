f63c697f0781c2ff7eb8b99160bf26a16b2d0e70
Irityan <thinkabout42@yandex.ru>
2019-12-02 05:46:27 +0300

    Объединение JSON (сырой вариант)

---
+++ uniteJSON.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import JSONHelper, FileHelper
+
+paths = ["..\\ML-Text-Project DATA\\ISKurasov.json", "..\\ML-Text-Project DATA\\AVSafeeva.json"]
+output = "..\\ML-Text-Project DATA\\allReviews.json"
+
+data = JSONHelper.mergeFiles(paths, output)
